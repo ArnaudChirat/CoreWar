33b1d35d0945025614b277a31313d87f438e544b
lbelda <lbelda@student.42.fr>
2018-06-27 08:25:56 +0200

    [Leaks] Cleaned the SDL in a better way

--- srcs/corewar/visu/utils/clean.c
+++ srcs/corewar/visu/utils/clean.c
@@ -6,7 +6,7 @@
 /*   By: lbelda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/02 11:38:32 by lbelda            #+#    #+#             */
-/*   Updated: 2018/06/27 07:50:05 by lbelda           ###   ########.fr       */
+/*   Updated: 2018/06/27 08:24:25 by lbelda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,12 @@ void		ft_free_visu(t_visu *v, t_data d)
 	{
 		free_scene(&v->scene);
 		free_sound(&v->sound);
+		SDL_PumpEvents();
+		SDL_FlushEvents(0, SDL_LASTEVENT);
+		while (SDL_PollEvent(NULL))
+		{
+		}
+		SDL_GameControllerClose(v->controls.ps3_id);
 		SDL_GL_DeleteContext(v->context);
 		SDL_DestroyWindow(v->win);
 		SDL_QuitSubSystem(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
